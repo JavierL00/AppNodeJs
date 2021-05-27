1f952ddcf47685af8a5b9d3dc355d86099026fb9
Javier Luján <javier.lujan.121@outlook.es>
2021-05-26 23:42:21 -0500

    Creating controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { PostsController } from './posts/posts.controller';
+import { CategoriesController } from './categories/categories.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController, PostsController, CategoriesController],
   providers: [AppService],
 })
 export class AppModule {}
